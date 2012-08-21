5c785d5aef6ea059a8fadb79c5be913f1b9c3fa2
Jordan Scales <scalesjordan@gmail.com>
2012-08-20 21:03:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilictronix::Application.config.secret_token = '4f25d45fda03d2447967b38fb41a74ecb69c838457fec7f011310216934f9793ce1f3c97ab4e2cc28065fcf7188a5938e373f86e883469af6839db6f64fd11b4'
