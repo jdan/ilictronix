33b5aeb6432149db9bbd3a19ea51e0c0d2566faa
Jordan Scales <scalesjordan@gmail.com>
2012-08-21 22:39:26 -0400

    home controller and main layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Ilictronix::Application.routes.draw do
 
   resources :users
   resources :sessions
+  resources :home
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +57,7 @@ Ilictronix::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
