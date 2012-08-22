71ca3c5c9a9c0b58483b4d8739d4c7c6f0899c1a
Jordan Scales <scalesjordan@gmail.com>
2012-08-21 22:33:24 -0400

    login and signup forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+
+  attr_accessible :username, :email, :password, :password_confirmation
+
+  validates_confirmation_of :password
+  validates_presence_of :username
+  validates_presence_of :email
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
+
 end
