db9b405e1ddb46086fdd4245f7d3744c9bce847e
Jordan Scales <scalesjordan@gmail.com>
2012-08-22 23:53:47 -0400

    methods for getting a short month and day

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :title, :post_type
 
   default_scope order('created_at DESC')
+
+  def month
+    self.created_at.strftime('%b').upcase
+  end
+
+  def day
+    self.created_at.strftime('%d')
+  end
 end
