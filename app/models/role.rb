a31ccc1659c2ca68f82ed2206283a422f6887681
Jordan Scales <scalesjordan@gmail.com>
2012-08-27 14:41:23 -0400

    added role table

---
+++ app/models/role.rb
@@ -0,0 +1,3 @@
+class Role < ActiveRecord::Base
+  attr_accessible :title
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120824032923) do
+ActiveRecord::Schema.define(:version => 20120827184047) do
 
   create_table "posts", :force => true do |t|
     t.integer  "author"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(:version => 20120824032923) do
     t.integer  "score",      :default => 0
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "user_role", :id => false, :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
   end
 
   create_table "users", :force => true do |t|
