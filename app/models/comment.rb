9eafc77fef38dd01b2cda8d9343c7996d9172341
Jordan Scales <scalesjordan@gmail.com>
2012-09-03 14:59:07 -0400

    validations on post and comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   attr_accessible :body, :post_id
+
+  validates_presence_of :body, :post_id, :user_id
+  validates_length_of :body, :minimum => 10, :maximum => 1000
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ActiveRecord::Base
 
   attr_accessible :content, :title, :post_type
 
+  validates_presence_of :content, :title, :post_type
+  validates_length_of :content, :minimum => 10, :maximum => 2000
+  validates_length_of :title, :maximum => 100
+
   default_scope order('created_at DESC')
 
   def month
