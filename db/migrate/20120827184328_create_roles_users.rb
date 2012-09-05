19baa094e77d45171f0ca0a58ed8a1148d08df28
Jordan Scales <scalesjordan@gmail.com>
2012-09-04 20:13:27 -0400

    changed class name in roles_users migration

--- db/migrate/20120827184328_create_roles_users.rb
+++ db/migrate/20120827184328_create_roles_users.rb
@@ -1,4 +1,4 @@
-class CreateUserRole < ActiveRecord::Migration
+class CreateRolesUsers < ActiveRecord::Migration
   def up
     create_table :roles_users, :id => false do |t|
       t.references :user, :role
