503527a1a43cd70becf603578d89b141bd673104
Jordan Scales <scalesjordan@gmail.com>
2012-09-22 13:09:02 -0400

    gravatar URL helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
     Redcarpet.new(text).to_html.html_safe
   end
 
+  # gravatar
+  def avatar_url(user)
+    default_url = "#{root_url}assets/default_user.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=80&d=#{CGI.escape(default_url)}"
+  end
 end
