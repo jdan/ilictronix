5a3a9e28d3d219172e0ec67fde9487441d538e5e
Jordan Scales <scalesjordan@gmail.com>
2012-09-22 14:36:17 -0400

    added markdown in comments, better comment styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,6 @@ module ApplicationHelper
   def avatar_url(user)
     default_url = "#{root_url}assets/default_user.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=80&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=64&d=#{CGI.escape(default_url)}"
   end
 end
