ad30a5e88b522a54d80592ac2eb5bbbd9bf26b6d
Jordan Scales <scalesjordan@gmail.com>
2013-05-30 12:18:56 -0400

    refactored string extensions to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'json'
+
 module ApplicationHelper
 
   # renders markdown, escaping premature HTML code before-hand
@@ -17,4 +20,30 @@ module ApplicationHelper
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=64&d=#{CGI.escape(default_url)}"
   end
 
+end
+
+# TODO: better naming convention
+
+class String
+  # convert spotify URLs to embed code
+  def to_spotify
+    self.gsub(/^http:\/\/open.spotify.com\/track\/(.+)$/, 
+      "<iframe src=\"https://embed.spotify.com/?uri=spotify:track:\\1\" width=\"550\" height=\"80\" frameborder=\"0\" allowtransparency=\"true\"></iframe>")
+  end
+
+  # convert soundcloud URLs to embed code
+  def to_soundcloud
+    # makes JSON call to soundcloud API to get embed code from URL
+    # TODO: cache
+    def fetch_code(url)
+      fmt = URI.encode("http://soundcloud.com/oembed?format=json&url=#{url}&iframe=true")
+      JSON.parse(Net::HTTP.get(URI.parse(fmt)))['html'] rescue url
+    end
+    self.gsub(/^https?:\/\/soundcloud.com\/(.+)\/(.+)$/) { |m| fetch_code m }
+  end
+
+  # convert hashtags to links
+  def hash_links
+    self.gsub(/#([a-zA-Z]+)\s*/, "<a href=\"/tagged/\\1\">#\\1</a> ")
+  end
 end
